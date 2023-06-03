d1c5e3f17f88efb2222382db1c60fb03cb3bef84
muham <muham@DESKTOP-KS7TFJT>
2023-06-03 19:50:34 +0700

    #update 24 juneAdd project files.

---
+++ PokemonReviewApp/Repository/PokemonRepository.cs
@@ -0,0 +1,46 @@
+﻿using PokemonReviewApp.Data;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+using System.Runtime.CompilerServices;
+
+namespace PokemonReviewApp.Repository
+{
+    public class PokemonRepository : IPokemonRepository
+    {
+        private readonly DataContext _context;
+        public PokemonRepository(DataContext context) 
+        {
+            _context = context; 
+        }
+
+        public Pokemon GetPokemon(int id)
+        {
+            return _context.Pokemons.Where(p => p.Id == id).FirstOrDefault();
+        }
+
+        public Pokemon GetPokemon(string name)
+        {
+            return _context.Pokemons.Where(p => p.Name == name).FirstOrDefault();
+        }
+
+        public decimal GetPokemonRating(int pokeId)
+        {
+            var review = _context.Reviews.Where(p => p.Pokemon.Id == pokeId);
+
+            if (!review.Any())
+                return 0;
+
+            return ((decimal)review.Sum(r => r.Rating) / review.Count());
+        }
+
+        public ICollection<Pokemon> GetPokemons()
+        {
+            return _context.Pokemons.OrderBy(p => p.Id).ToList();
+        }
+
+        public bool PokemonExists(int pokeId)
+        {
+            return _context.Pokemons.Any(p => p.Id == pokeId);
+        }
+    }
+}
