cf0b84d03407ab32f906efb2c17371c51a5b244d
muham <muham@DESKTOP-KS7TFJT>
2023-06-03 19:50:34 +0700

    Add project files.

---
+++ PokemonReviewApp/Models/Owner.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.NetworkInformation;
+
+namespace PokemonReviewApp.Models
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; internal set; }
+        public string Gym { get; set; }
+        public Country Country { get; set; }
+        public ICollection<PokemonOwner> PokemonOwners { get; set; }
+
+    }
+}
